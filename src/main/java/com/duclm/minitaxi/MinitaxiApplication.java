ddcb4849e7f0b6f8d8c2f98d6700ff71eaf4029a
Duclm0310 <88842378+Duckday01@users.noreply.github.com>
2025-12-30 22:20:47 +0700

    init backend + docker mysql redis

---
+++ src/main/java/com/duclm/minitaxi/MinitaxiApplication.java
@@ -0,0 +1,13 @@
+package com.duclm.minitaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinitaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinitaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duclm/minitaxi/MinitaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duclm.minitaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinitaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
